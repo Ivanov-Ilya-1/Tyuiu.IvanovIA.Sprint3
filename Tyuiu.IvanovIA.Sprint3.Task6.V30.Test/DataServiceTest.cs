87b953d6b354621244177318cebfd9482c66c25d
tyuiu.ivanovia@outlook.com <gilya0407@gmail.com>
2025-10-28 17:11:41 +0300

    Task6

---
+++ Tyuiu.IvanovIA.Sprint3.Task6.V30.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.IvanovIA.Sprint3.Task6.V30.Lib;
+namespace Tyuiu.IvanovIA.Sprint3.Task6.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int value = 25;
+            int startvalue1 = 11;
+            int stopvalue1 = 17;
+            
+
+            int res = ds.GetSumTheDivisors(startvalue1, stopvalue1);
+
+            Assert.AreEqual(value, res);
+        }
+    }
+}
---
+++ Tyuiu.IvanovIA.Sprint3.Task6.V30/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.IvanovIA.Sprint3.Task6.V30.Lib;
+namespace Tyuiu.IvanovIA.Sprint3.Task6.V30
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+
+            Console.Title = "Спринт #3 | Выполнил: Иванов И. А. | ИИПБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" Спринт #3                                                                *");
+            Console.WriteLine(" Тема : Оператор цикла foreach                                            *");
+            Console.WriteLine(" Задание #4                                                               *");
+            Console.WriteLine(" Вариант #16                                                               *");
+            Console.WriteLine(" Выполнил : Иванов Илья Анатольевич | ИИПБ-25-1                           *");
+            Console.WriteLine(" Спринт #3                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" УСЛОВИЕ:                                                                 *");
+            Console.WriteLine(" Используя цикл foreach удалить все буквы и знаки препинания,оставить цифр*");
+            Console.WriteLine(" затем преобразовать в число в строке: vc,x12fc fc!e                      *");
+            Console.WriteLine(" *                                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            
+            int startvalue1 = 11;
+            int stopvalue1 = 17;
+            
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("Количество = " + ds.GetSumTheDivisors(startvalue1,stopvalue1));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
